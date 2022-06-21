4e1ef8d39fbfe792d24419faf65986d450eaa70f
Asyps <105502574+Asyps@users.noreply.github.com>
2022-06-21 12:25:02 +0200

    Add files via upload

---
+++ chess.py
@@ -0,0 +1,90 @@
+board = {
+    # black
+    (1, 1): {
+        "type": "rook",
+        "color": "black"
+    },
+    (2, 1): {
+        "type": "knight",
+        "color": "black"
+    },
+    (3, 1): {
+        "type": "bishop",
+        "color": "black"
+    },
+    (4, 1): {
+        "type": "queen",
+        "color": "black"
+    },
+    (5, 1): {
+        "type": "king",
+        "color": "black"
+    },
+    (6, 1): {
+        "type": "bishop",
+        "color": "black"
+    },
+    (7, 1): {
+        "type": "knight",
+        "color": "black"
+    },
+    (8, 1): {
+        "type": "rook",
+        "color": "black"
+    },
+    
+    # white
+    (1, 8): {
+        "type": "rook",
+        "color": "white"
+    },
+    (2, 8): {
+        "type": "knight",
+        "color": "white"
+    },
+    (3, 8): {
+        "type": "bishop",
+        "color": "white"
+    },
+    (4, 8): {
+        "type": "queen",
+        "color": "white"
+    },
+    (5, 8): {
+        "type": "king",
+        "color": "white"
+    },
+    (6, 8): {
+        "type": "bishop",
+        "color": "white"
+    },
+    (7, 8): {
+        "type": "knight",
+        "color": "white"
+    },
+    (8, 8): {
+        "type": "rook",
+        "color": "white"
+    }   
+}
+
+for i in range(1, 9):
+    board[(i, 2)] = {
+        "type": "pawn",
+        "color": "black",
+        "moved": False
+    },
+    board[(i, 7)] = {
+        "type": "pawn",
+        "color": "white",
+        "moved": False
+    },
+
+for i in range(1, 9):
+    for j in range(1, 9):
+        coord = (i, j)
+        if coord in board:
+            print("  ")
+        else:
+            print(board[coord]["type"][:2], end="")
+    print()
